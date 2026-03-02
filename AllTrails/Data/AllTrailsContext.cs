5aef20ac0630821c1db6d98aa08f3706717a5794
Michael Trumbull <michael.trumbull@nscc.ca>
2026-03-02 14:06:44 -0400

    Add project files.

---
+++ AllTrails/Data/AllTrailsContext.cs
@@ -0,0 +1,22 @@
+﻿using AllTrails.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllTrails.Data
+{
+    public class AllTrailsContext : IdentityDbContext<IdentityUser, IdentityRole, string> // switched to IdentityDbContext
+    {
+        public AllTrailsContext (DbContextOptions<AllTrailsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AllTrails.Models.Trail> Trail { get; set; } = default!;
+        public DbSet<AllTrails.Models.TripReport> TripReport { get; set; } = default!;
+    }
+}
---
+++ AllTrails/Program.cs
@@ -0,0 +1,62 @@
+using AllTrails.Data;
+using AllTrails.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AllTrailsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AllTrailsContext") ?? throw new InvalidOperationException("Connection string 'AllTrailsContext' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false) // changed to false
+    .AddRoles<IdentityRole>() // add support for roles
+    .AddEntityFrameworkStores<AllTrailsContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add custom service Azure Blob Service
+builder.Services.AddScoped<AzureBlobService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+// Add Identity Razor Pages
+app.MapRazorPages().WithStaticAssets();
+
+//
+// Seed Identity roles in the app
+//
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    string[] roles = { "Admin", "Reviewer" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+
+app.Run();
